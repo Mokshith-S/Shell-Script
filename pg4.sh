5f1823f00bddd0d8738c26dd5e3e2af8e7197a03
Mokshith-S <79778510+Mokshith-S@users.noreply.github.com>
2022-02-25 16:01:52 +0530

    Update pg4.sh

--- pg4.sh
+++ pg4.sh
@@ -1,5 +1,7 @@
-/* 4a. Write a shell script that accept one or more file names as argument and convert all of them
+<<comment 
+4a. Write a shell script that accept one or more file names as argument and convert all of them
 to uppercase, provided they exists in current directory. */
+comment
 
 for i in $*
 do
