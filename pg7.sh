611d807ce1f0461d019fca2bc8f86750872ff720
Mokshith-S <mokshithnaikputtur@gmail.com>
2022-03-07 15:41:34 +0530

    output

--- pg7.sh
+++ pg7.sh
@@ -3,16 +3,19 @@
 “Good Afternoon” or “Good Evening” depending upon time at which the user logs in. */
 comment
 
+set -- `who`
+user=$1
+set -- `echo $4 | tr ";" " "`
 h=`date +%H`
 if [ $h -ge 4 ] && [ $h -lt 12 ]
 then
-echo "Good morning Mr./Ms. $U"
+echo "Good Morning $user"
 elif [ $h -ge 12 ] && [ $h -lt 16 ]
 then
-echo "Good Afternoon"
+echo "Good Afternoon $user"
 elif [ $h -ge 16 ] && [ $h -lt 19 ]
 then
-echo "Good evening"
+echo "Good Evening $user"
 else
-echo "Goodnight"
+echo "Good  Night $user"
 fi
