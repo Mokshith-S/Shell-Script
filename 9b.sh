53f307ecce8b7d9774cde9396883728777709561
Mokshith-S <mokshithnaikputtur@gmail.com>
2022-04-05 16:47:27 +0530

    First version

---
+++ 9b.sh
@@ -0,0 +1,26 @@
+echo "Enter the file name:"
+read file
+if [ -f $file ]
+then 
+while read line
+do	
+	start=1
+	end=40
+	length=`expr length "$line"`
+	while [ $length -ge 40 ]
+	do
+		
+		echo -n "`echo $line | cut -c $start-$end`\\"
+		length=`expr $length - $end`
+		start=`expr $end  + 1`
+		end=`expr $end + 40`
+		
+	done 
+	echo -n "`echo $line | cut -c $start-`\\"
+	echo ""
+
+
+done < $file
+else
+	echo "$file doesn't exists"
+fi
